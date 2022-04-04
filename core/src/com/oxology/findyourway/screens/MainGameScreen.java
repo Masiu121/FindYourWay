26ac5cd1ce4db14229782707a4a90cfc71ad6930
Masiu121 <maks.kapolka@gmail.com>
2022-04-04 21:06:38 +0200

    Added MainGameScreen

---
+++ core/src/com/oxology/findyourway/screens/MainGameScreen.java
@@ -0,0 +1,41 @@
+package com.oxology.findyourway.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainGameScreen implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 1);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
