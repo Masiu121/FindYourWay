bb5553953ad600217e8b19605f6d79a4bfb521f6
Masiu121 <maks.kapolka@gmail.com>
2022-04-04 21:06:48 +0200

    Updated MainMenuScreen

--- core/src/com/oxology/findyourway/screens/MainMenuScreen.java
+++ core/src/com/oxology/findyourway/screens/MainMenuScreen.java
@@ -1,9 +1,9 @@
 package com.oxology.findyourway.screens;
 
-import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.oxology.findyourway.FindYourWay;
 import com.oxology.findyourway.GameTexture;
 import com.oxology.findyourway.utils.Button;
@@ -11,7 +11,10 @@ import com.oxology.findyourway.utils.Button;
 public class MainMenuScreen implements Screen {
     FindYourWay game;
     BitmapFont font;
-    float scale;
+
+    Button playButton;
+    Button creditsButton;
+    Button exitButton;
 
     public MainMenuScreen(FindYourWay game) {
         this.game = game;
@@ -19,19 +22,40 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void show() {
-        font = new BitmapFont(/*Gdx.files.internal("Menu/PixelFont.ttf")*/);
-        scale = 5;
+        font = new BitmapFont(Gdx.files.internal("Menu/PixelFont.fnt"));
+
+        playButton = new Button(game, 0, 0, 4, "Play", new MainGameScreen());
+        creditsButton = new Button(game, 0, 0, 4, "Credits", new CreditsScreen(game));
+        exitButton = new Button(game, 0, 0, 4, "Exit", null);
+
+        int buttonX = Gdx.graphics.getBackBufferWidth()/2-playButton.getWidth()/2;
+
+        playButton.move(buttonX, 350);
+        creditsButton.move(buttonX, 200);
+        exitButton.move(buttonX, 50);
     }
 
     @Override
     public void render(float delta) {
+        update();
+        ScreenUtils.clear(0, 0, 0, 1);
+
         game.batch.begin();
+
         game.batch.draw(GameTexture.MENU_BACKGROUND, 0, 0, FindYourWay.WINDOW_WIDTH, FindYourWay.WINDOW_HEIGHT);
 
-        Button button = new Button(10, 10, 1);
-        button.draw(game.batch);
-        font.draw(game.batch, "Version: " + FindYourWay.VERSION, 100, 100);
+        playButton.draw(game.batch);
+        creditsButton.draw(game.batch);
+        exitButton.draw(game.batch);
+        font.draw(game.batch, "Version: " + FindYourWay.VERSION, 1, 25);
+
         game.batch.end();
+    }
+
+    public void update() {
+        playButton.update();
+        creditsButton.update();
+        exitButton.update();
     }
 
     @Override
