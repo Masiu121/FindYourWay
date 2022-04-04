df158c0f42c370868a8723a9219a8e6e1960ceb2
Masiu121 <maks.kapolka@gmail.com>
2022-04-04 21:06:30 +0200

    Updated GameTexture

--- core/src/com/oxology/findyourway/GameTexture.java
+++ core/src/com/oxology/findyourway/GameTexture.java
@@ -5,4 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 public interface GameTexture {
     Texture MENU_BACKGROUND = new Texture("Menu/MenuBackground.png");
     Texture MENU_BUTTON = new Texture("Menu/MenuButton.png");
+    Texture MENU_BUTTON_HOVER = new Texture("Menu/MenuButtonHover.png");
+    Texture MENU_BUTTON_CLICK = new Texture("Menu/MenuButton.png");
 }
