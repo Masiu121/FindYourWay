e3304a986c76493af4c595755ca4ef7af744303d
Masiu121 <maks.kapolka@gmail.com>
2022-04-05 18:54:07 +0200

    Updated MainGameScreen

--- core/src/com/oxology/findyourway/screens/MainGameScreen.java
+++ core/src/com/oxology/findyourway/screens/MainGameScreen.java
@@ -1,17 +1,44 @@
 package com.oxology.findyourway.screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.oxology.findyourway.FindYourWay;
+import com.oxology.findyourway.world.entities.Entity;
 
 public class MainGameScreen implements Screen {
-    @Override
-    public void show() {
+    FindYourWay game;
+    OrthographicCamera camera;
 
+    Entity player;
+
+    public MainGameScreen(FindYourWay game) {
+        this.game = game;
     }
 
     @Override
-    public void render(float delta) {
+    public void show() {
+        camera = new OrthographicCamera(480, 270);
+        camera.position.set(camera.viewportWidth/2f, camera.viewportHeight/2f, 0);
+        camera.update();
+
+        player = new Entity(10, 10, null);
+    }
+
+    @Override
+    public void render(float deltaTime) {
+        update(deltaTime);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
         ScreenUtils.clear(0, 0, 0, 1);
+
+        player.draw(game.batch);
+    }
+
+    public void update(float deltaTime) {
+        player.update(deltaTime);
     }
 
     @Override
