7eae55caf8860f1c0bb2a639efa6b78cf66eda4a
Masiu121 <maks.kapolka@gmail.com>
2022-04-05 18:54:00 +0200

    Updated FindYourWay

--- core/src/com/oxology/findyourway/FindYourWay.java
+++ core/src/com/oxology/findyourway/FindYourWay.java
@@ -1,13 +1,19 @@
 package com.oxology.findyourway;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.oxology.findyourway.screens.MainMenuScreen;
 
 public class FindYourWay extends Game {
-	public static final int WINDOW_WIDTH = 1280;
-	public static final int WINDOW_HEIGHT = 720;
-	public static final float HW_PROP = (float)WINDOW_HEIGHT/WINDOW_WIDTH;
+	public static final int VIEWPORT_WIDTH = 480;
+	public static final int VIEWPORT_HEIGHT = 270;
+
+	public static final int WINDOW_WIDTH = 1920;
+	public static final int WINDOW_HEIGHT = 1080;
+
+	public static final float WINDOW_VIEWPORT_Y_PROP = (float) WINDOW_HEIGHT/VIEWPORT_HEIGHT;
+	public static final float WINDOW_VIEWPORT_X_PROP = (float) WINDOW_WIDTH/VIEWPORT_WIDTH;
 
 	public static final String VERSION = "0.01a";
 
@@ -15,6 +21,7 @@ public class FindYourWay extends Game {
 
 	@Override
 	public void create () {
+		//Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
 		batch = new SpriteBatch();
 		this.setScreen(new MainMenuScreen(this));
 	}
