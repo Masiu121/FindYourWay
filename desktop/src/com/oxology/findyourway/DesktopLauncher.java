e92816e466ce3d14446297fb177d0b673ba8817a
Masiu121 <maks.kapolka@gmail.com>
2022-04-05 18:53:52 +0200

    Updated DesktopLauncher

--- desktop/src/com/oxology/findyourway/DesktopLauncher.java
+++ desktop/src/com/oxology/findyourway/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package com.oxology.findyourway;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.oxology.findyourway.FindYourWay;
@@ -9,6 +11,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setResizable(false);
+		//config.setFullscreenMode(Gdx.graphics.getDisplayMode());
 		config.setWindowedMode(FindYourWay.WINDOW_WIDTH, FindYourWay.WINDOW_HEIGHT);
 		config.setForegroundFPS(60);
 		new Lwjgl3Application(new FindYourWay(), config);
