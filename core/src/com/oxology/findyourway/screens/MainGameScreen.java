c64f1de006ab8a965d2a6bd24c45d3cf34a5cdec
Masiu121 <maks.kapolka@gmail.com>
2022-04-07 00:24:18 +0200

    Updated MainGameScreen

--- core/src/com/oxology/findyourway/screens/MainGameScreen.java
+++ core/src/com/oxology/findyourway/screens/MainGameScreen.java
@@ -1,9 +1,13 @@
 package com.oxology.findyourway.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.oxology.findyourway.FindYourWay;
+import com.oxology.findyourway.GameTexture;
 import com.oxology.findyourway.world.entities.Entity;
 
 public class MainGameScreen implements Screen {
@@ -11,6 +15,9 @@ public class MainGameScreen implements Screen {
     OrthographicCamera camera;
 
     Entity player;
+    TextureRegion[][] animationFrames;
+    Animation<TextureRegion> animation;
+    float timeElapsed;
 
     public MainGameScreen(FindYourWay game) {
         this.game = game;
@@ -22,19 +29,26 @@ public class MainGameScreen implements Screen {
         camera.position.set(camera.viewportWidth/2f, camera.viewportHeight/2f, 0);
         camera.update();
 
-        player = new Entity(10, 10, null);
+        player = new Entity(10, 10, GameTexture.MAIN_CHAR_IDLE);
+
+        animationFrames = TextureRegion.split(player.getTexture(), 14, 45);
+        animation = new Animation<TextureRegion>(1f/4f, animationFrames[0]);
     }
 
     @Override
     public void render(float deltaTime) {
-        update(deltaTime);
+        timeElapsed += Gdx.graphics.getDeltaTime();
+
+        update(Gdx.graphics.getDeltaTime());
 
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
         ScreenUtils.clear(0, 0, 0, 1);
 
-        player.draw(game.batch);
+        game.batch.begin();
+        game.batch.draw(animation.getKeyFrame(timeElapsed, true), player.getX(), player.getY());
+        game.batch.end();
     }
 
     public void update(float deltaTime) {
