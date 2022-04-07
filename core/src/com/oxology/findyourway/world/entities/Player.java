919b687741a47f1dcecfb04de953b66d0a1533a4
Masiu121 <maks.kapolka@gmail.com>
2022-04-07 14:35:54 +0200

    Updated Player

--- core/src/com/oxology/findyourway/world/entities/Player.java
+++ core/src/com/oxology/findyourway/world/entities/Player.java
@@ -3,58 +3,109 @@ package com.oxology.findyourway.world.entities;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.oxology.findyourway.FindYourWay;
+import com.oxology.findyourway.GameTexture;
 
 public class Player extends Entity {
     private boolean jump;
     private boolean touchingGround;
 
-    private float xSpeed;
-    private float ySpeed;
+    TextureRegion[][] idleAnimationFrames1;
+    Animation<TextureRegion> idleAnimation1;
 
-    private final float defaultXSpeed;
-    private final float defaultYSpeed;
+    TextureRegion[][] idleAnimationFrames2;
+    Animation<TextureRegion> idleAnimation2;
+
+    TextureRegion[][] walkAnimationFrames1;
+    Animation<TextureRegion> walkAnimation1;
+
+    TextureRegion[][] walkAnimationFrames2;
+    Animation<TextureRegion> walkAnimation2;
+
+
+
+    private int animationStatus;
+    /*
+    0 - idle
+    1 - walk
+    2 - run
+    3 - jump
+     */
+
+    private int direction;
+    /*
+    0 - right
+    1 - left
+     */
 
     public Player(int x, int y, Texture texture, float scale) {
-        super(x, y, texture, scale, true, 9);
+        super(x, y, texture, scale, 9, true);
 
-        this.defaultXSpeed = 80f*scale;
-        this.defaultYSpeed = 150f*scale;
+        this.animationStatus = 0;
+        this.direction = 0;
 
-        xSpeed = 0;
+        this.idleAnimationFrames1 = TextureRegion.split(GameTexture.MAIN_CHAR_IDLE_1, 14, 45);
+        this.idleAnimation1 = new Animation<TextureRegion>(1f/4f, idleAnimationFrames1[0]);
+
+        this.idleAnimationFrames2 = TextureRegion.split(GameTexture.MAIN_CHAR_IDLE_2, 14, 45);
+        this.idleAnimation2 = new Animation<TextureRegion>(1f/4f, idleAnimationFrames2[0]);
+
+        this.walkAnimationFrames1 = TextureRegion.split(GameTexture.MAIN_CHAR_WALK_1, 20, 45);
+        this.walkAnimation1 = new Animation<TextureRegion>(1f/8f, walkAnimationFrames1[0]);
+
+        this.walkAnimationFrames2 = TextureRegion.split(GameTexture.MAIN_CHAR_WALK_2, 20, 45);
+        this.walkAnimation2 = new Animation<TextureRegion>(1f/8f, walkAnimationFrames2[0]);
 
         this.jump = false;
     }
 
     public void update(float deltaTime) {
         if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-            this.xSpeed = -defaultXSpeed;
+            super.setxSpeed(-super.getDefaultXSpeed());
+            direction = 0;
         } else if(Gdx.input.isKeyPressed(Input.Keys.D)) {
-            this.xSpeed = defaultXSpeed;
+            super.setxSpeed(super.getDefaultXSpeed());
+            direction = 1;
         } else {
-            this.xSpeed = 0;
+            super.setxSpeed(0);
         }
 
         if(Gdx.input.isKeyJustPressed(Input.Keys.W) && touchingGround) {
-            ySpeed = defaultYSpeed;
+            super.setySpeed(super.getDefaultYSpeed());
             this.jump = true;
         }
 
-        if(super.getY() == 10) {
+        if(super.getY() == 3) {
             touchingGround = true;
         }
 
         if(jump) {
             touchingGround = false;
-            ySpeed = ySpeed - FindYourWay.GRAVITY;
-            if(super.getY()+ySpeed*deltaTime < 10) {
+            super.setySpeed(super.getySpeed() - FindYourWay.GRAVITY);
+            if(super.getY()+super.getySpeed()*deltaTime < 3) {
                 jump = false;
-                move((int) super.getX(), 10);
-                ySpeed = 0;
+                move((int) super.getX(), 3);
+                super.setySpeed(0);
             }
         }
 
-        move(xSpeed*deltaTime, ySpeed*deltaTime);
+        if(super.isMoving()) {
+            if(direction == 0) {
+                setAnimation(walkAnimation1, 20, 45, true);
+            } else if(direction == 1) {
+                setAnimation(walkAnimation2, 20, 45, true);
+            }
+        } else {
+            if(direction == 0) {
+                setAnimation(idleAnimation1, 14, 45, true);
+            } else if(direction == 1) {
+                setAnimation(idleAnimation2, 14, 45, true);
+            }
+        }
+
+        move(super.getxSpeed()*deltaTime, super.getySpeed()*deltaTime);
 
         super.update(deltaTime);
     }
