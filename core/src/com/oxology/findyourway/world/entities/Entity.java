fef66639c2bb77fb096dffeeee6df52cf975cd11
Masiu121 <maks.kapolka@gmail.com>
2022-04-07 08:29:52 +0200

    Updated Entity

--- core/src/com/oxology/findyourway/world/entities/Entity.java
+++ core/src/com/oxology/findyourway/world/entities/Entity.java
@@ -3,11 +3,16 @@ package com.oxology.findyourway.world.entities;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.oxology.findyourway.FindYourWay;
 import com.oxology.findyourway.utils.GameObject;
 
 public class Entity extends GameObject {
 
     private boolean jump;
+    private boolean touchingGround;
 
     private float xSpeed;
     private float ySpeed;
@@ -15,32 +20,58 @@ public class Entity extends GameObject {
     private final float defaultXSpeed;
     private final float defaultYSpeed;
 
-    public Entity(int x, int y, Texture texture) {
-        super(x, y, texture);
+    TextureRegion[][] animationFrames;
+    Animation<TextureRegion> animation;
+    float timeElapsed;
 
-        this.defaultXSpeed = 100f;
-        this.defaultYSpeed = 500f;
+    public Entity(int x, int y, Texture texture, float scale) {
+        super(x, y, texture, scale);
+
+        this.defaultXSpeed = 100f*scale;
+        this.defaultYSpeed = 200f*scale;
 
         xSpeed = 0;
 
         this.jump = false;
+
+        animationFrames = TextureRegion.split(super.getTexture(), 14, 45);
+        animation = new Animation<TextureRegion>(1f/4f, animationFrames[0]);
     }
 
     public void update(float deltaTime) {
+        timeElapsed += deltaTime;
+
         if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-                this.xSpeed = -defaultXSpeed;
+            this.xSpeed = -defaultXSpeed;
         } else if(Gdx.input.isKeyPressed(Input.Keys.D)) {
-                this.xSpeed = defaultXSpeed;
+            this.xSpeed = defaultXSpeed;
         } else {
             this.xSpeed = 0;
         }
 
-        if(Gdx.input.isKeyPressed(Input.Keys.W)) {
+        if(Gdx.input.isKeyJustPressed(Input.Keys.W) && touchingGround) {
+            ySpeed = defaultYSpeed;
             this.jump = true;
         }
 
-        move((int) (getX() + xSpeed*deltaTime), (int) (getY() + ySpeed*deltaTime));
+        if(super.getY() == 10) {
+            touchingGround = true;
+        }
 
-        System.out.println(xSpeed*deltaTime);
+        if(jump) {
+            touchingGround = false;
+            ySpeed = ySpeed - FindYourWay.GRAVITY;
+            if(super.getY()+ySpeed*deltaTime < 10) {
+                jump = false;
+                move((int) super.getX(), 10);
+                ySpeed = 0;
+            }
+        }
+
+        move(xSpeed*deltaTime, ySpeed*deltaTime);
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(animation.getKeyFrame(timeElapsed, true), super.getX(), super.getY(), 14*super.getScale(), 45*super.getScale());
     }
 }
