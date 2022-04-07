306a9d2d3b73b83fb646ad52cc000aa4fa56415e
Masiu121 <maks.kapolka@gmail.com>
2022-04-07 09:08:53 +0200

    Updated GameTexture

--- core/src/com/oxology/findyourway/GameTexture.java
+++ core/src/com/oxology/findyourway/GameTexture.java
@@ -7,4 +7,6 @@ public interface GameTexture {
     Texture MENU_BUTTON = new Texture("Menu/MenuButton.png");
     Texture MENU_BUTTON_HOVER = new Texture("Menu/MenuButtonHover.png");
     Texture MAIN_CHAR_IDLE = new Texture("Game/Sprites/MainCharacterIdle.png");
+    Texture BARREL = new Texture("Game/Sprites/Barrel.png");
+    Texture FIRE = new Texture("Game/Sprites/Fire.png");
 }
