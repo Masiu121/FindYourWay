38580a647898f8d7d015f27b7e7d34f307dfa201
Masiu121 <maks.kapolka@gmail.com>
2022-04-10 22:35:26 +0200

    Updated Barrel

--- core/src/com/oxology/findyourway/world/entities/Barrel.java
+++ core/src/com/oxology/findyourway/world/entities/Barrel.java
@@ -1,14 +1,21 @@
 package com.oxology.findyourway.world.entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.oxology.findyourway.GameTexture;
+import com.oxology.findyourway.FindYourWay;
+import com.oxology.findyourway.GameData;
+import com.oxology.findyourway.utils.CenteredText;
+import com.oxology.findyourway.utils.Message;
 
 public class Barrel extends Entity {
     private Entity fire;
     private boolean fired;
+    private boolean showText;
+    private Message message;
 
     TextureRegion[][] fireStartFrames;
     Animation<TextureRegion> fireStartAnimation;
@@ -21,33 +28,52 @@ public class Barrel extends Entity {
 
     private boolean fireStarted;
 
-    public Barrel(int x, int y, Texture texture, float scale) {
-        super(x, y, texture, scale, 1, false);
-        this.fire = new Entity((int) super.getX()-2, (int) super.getY()+19, GameTexture.FIRE, 1f, 5, true);
+    Sound fireStartSound;
+    Sound fireSound;
+    Sound zippoSound;
+    long fireSoundId;
+
+    public Barrel(int x, int y, Texture texture, float scale, FindYourWay game) {
+        super(x, y, texture, scale, 1, false, game);
+        this.fire = new Entity((int) super.getX()-2, (int) super.getY()+19, GameData.FIRE, 1f, 5, true, game);
         this.fired = false;
         this.fireStarted = false;
 
-        fireStartFrames = TextureRegion.split(GameTexture.FIRE_START, 21, 25);
+        this.message = new Message(50+GameData.BARREL.getWidth(), 30, "Press enter to fire it up!", 0.2f);
+
+        this.showText = false;
+
+        fireStartFrames = TextureRegion.split(GameData.FIRE_START, 21, 25);
         fireStartAnimation = new Animation<TextureRegion>(1f/12f, fireStartFrames[0]);
 
-        fireEndFrames = TextureRegion.split(GameTexture.FIRE_END, 21, 25);
+        fireEndFrames = TextureRegion.split(GameData.FIRE_END, 21, 25);
         fireEndAnimation = new Animation<TextureRegion>(1f/4f, fireEndFrames[0]);
 
-        fireFrames = TextureRegion.split(GameTexture.FIRE, 21, 25);
+        fireFrames = TextureRegion.split(GameData.FIRE, 21, 25);
         fireAnimation = new Animation<TextureRegion>(1f/8f, fireFrames[0]);
+        fire.setAnimation(null, 21, 25, false);
+
+        fireStartSound = Gdx.audio.newSound(GameData.FIRE_START_SOUND);
+        fireSound = Gdx.audio.newSound(GameData.FIRE_SOUND);
+        zippoSound = Gdx.audio.newSound(GameData.ZIPPO_SOUND);
 
         resumeAnimation();
     }
 
     public void toggleFire() {
-        if(fired) {
-            this.fire.setTimeElapsed(0);
-            this.fire.setAnimation(fireEndAnimation, 21, 25, false);
-            this.fireStarted = false;
-        } else {
-            this.fire.setTimeElapsed(0);
-            this.fire.setAnimation(fireStartAnimation, 21, 25, false);
-            this.fired = true;
+        if(fire.getAnimation() == null || fire.getAnimation() == fireAnimation) {
+            if (fired) {
+                fire.setTimeElapsed(0);
+                this.fire.setAnimation(fireEndAnimation, 21, 25, false);
+                this.fireStarted = false;
+                fireSound.setVolume(fireSoundId, 0.4f);
+            } else {
+                fire.setTimeElapsed(0);
+                this.fire.setAnimation(fireStartAnimation, 21, 25, false);
+                fireStartSound.play(1f);
+                zippoSound.play(1f);
+                this.fired = true;
+            }
         }
     }
 
@@ -56,22 +82,43 @@ public class Barrel extends Entity {
         if(fired) {
             fire.draw(batch);
         }
+
+        if(showText) {
+            message.draw(batch);
+        }
     }
 
     public void update(float deltaTime) {
         fire.update(deltaTime);
         super.update(deltaTime);
-        if(fire.getAnimation().isAnimationFinished(fire.getTimeElapsed())) {
+        if(fire.getAnimation() != null && fire.getAnimation().isAnimationFinished(fire.getTimeElapsed())) {
             if(fire.getAnimation() == fireStartAnimation) {
+                zippoSound.stop();
+                fireSoundId = fireSound.play();
+                fireSound.setLooping(fireSoundId, true);
                 fireStarted = true;
             }
             if(fire.getAnimation() == fireEndAnimation) {
+                fireSound.stop();
                 this.fired = false;
+                fire.setAnimation(null, 21, 25, false);
             }
         }
 
         if(fireStarted) {
             fire.setAnimation(fireAnimation, 21, 25, true);
         }
+    }
+
+    public void showText() {
+        this.showText = true;
+    }
+
+    public void hideText() {
+        this.showText = false;
+    }
+
+    public Message getMessage() {
+        return message;
     }
 }
