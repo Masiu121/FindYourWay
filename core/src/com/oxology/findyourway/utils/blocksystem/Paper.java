d9893f91aacf4cf157df6113225e8eb4530fcd7c
IvanGnatyszen <i.gnatishen@gmail.com>
2022-04-11 12:37:45 +0200

    Updated Paper

--- core/src/com/oxology/findyourway/utils/blocksystem/Paper.java
+++ core/src/com/oxology/findyourway/utils/blocksystem/Paper.java
@@ -4,11 +4,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.oxology.findyourway.GameData;
 
 public class Paper {
-    public Paper() {
-
-    }
-
-    public void draw(SpriteBatch batch) {
-        batch.draw(GameData.PAPER, 121, 2);
-    }
+//    public Paper() {
+//
+//    }
+//
+//    public void draw(SpriteBatch batch) {
+//        batch.draw(GameData.PAPER, 121, 2);
+//    }
 }
