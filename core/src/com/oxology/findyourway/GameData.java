e284a1a5e55d7accf51746f23d61c87cc1e34190
Masiu121 <maks.kapolka@gmail.com>
2022-04-11 16:03:22 +0200

    Added GameData

---
+++ core/src/com/oxology/findyourway/GameData.java
@@ -0,0 +1,43 @@
+package com.oxology.findyourway;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+public interface GameData {
+
+    FileHandle FONT = Gdx.files.internal("Menu/Font/PixelFont.fnt");
+
+    //Audio
+    FileHandle BACKGROUND_MUSIC = Gdx.files.internal("Menu/Audio/MenuMusic.mp3");
+    FileHandle FIRE_START_SOUND = Gdx.files.internal("Game/Audio/FireStartSound.mp3");
+    FileHandle FIRE_SOUND = Gdx.files.internal("Game/Audio/FireSound.mp3");
+    FileHandle ZIPPO_SOUND = Gdx.files.internal("Game/Audio/ZippoSound.mp3");
+
+    //Textures
+    Texture LOGO = new Texture("Menu/Logo.png");
+    Texture VIGNETTE = new Texture("Game/Vignette.png");
+    Texture MENU_BACKGROUND = new Texture("Menu/MenuBackground.png");
+    Texture MENU_BUTTON = new Texture("Menu/Buttons/MenuButton.png");
+    Texture MENU_BUTTON_HOVER = new Texture("Menu/Buttons/MenuButtonHover.png");
+    Texture GAME_BACKGROUND = new Texture("Game/Background.png");
+    Texture MAIN_CHAR_IDLE_1 = new Texture("Game/Sprites/MainCharacterIdle.png");
+    Texture MAIN_CHAR_IDLE_2 = new Texture("Game/Sprites/MainCharacterIdle2.png");
+    Texture MAIN_CHAR_WALK_1 = new Texture("Game/Sprites/MainCharacterWalk.png");
+    Texture MAIN_CHAR_WALK_2 = new Texture("Game/Sprites/MainCharacterWalk2.png");
+
+    Texture QUEST_MARK = new Texture("Game/Sprites/QuestMark.png");
+
+    Texture BARREL = new Texture("Game/Sprites/Barrel.png");
+    Texture FIRE_START = new Texture("Game/Sprites/FireStart.png");
+    Texture FIRE_END = new Texture("Game/Sprites/FireEnd.png");
+    Texture FIRE = new Texture("Game/Sprites/Fire.png");
+
+    Texture MESSAGE_RIGHT = new Texture("Game/Sprites/Message/MessageRight.png");
+    Texture MESSAGE_MIDDLE = new Texture("Game/Sprites/Message/MessageMiddle.png");
+
+    Texture START_BLOCK = new Texture("Game/Blocks/blockStart.png");
+
+    Texture PAPER = new Texture("Game/Sprites/Paper.png");
+}
