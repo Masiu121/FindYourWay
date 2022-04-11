1255f59825e91cf591da5084e4fe9e04d2629c5f
Masiu121 <maks.kapolka@gmail.com>
2022-04-11 16:03:39 +0200

    Added GameIntroScreen

---
+++ core/src/com/oxology/findyourway/screens/GameIntroScreen.java
@@ -0,0 +1,48 @@
+package com.oxology.findyourway.screens;
+
+import com.badlogic.gdx.Screen;
+import com.oxology.findyourway.FindYourWay;
+
+public class GameIntroScreen implements Screen {
+    FindYourWay game;
+
+    public GameIntroScreen(FindYourWay game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        game.mainGameScreen = new MainGameScreen(game);
+        game.setScreen(game.mainGameScreen);
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
