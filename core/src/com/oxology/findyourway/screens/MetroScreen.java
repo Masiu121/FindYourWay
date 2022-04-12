6656ac449068233091a127375b92a7fb0d5a4748
Masiu121 <maks.kapolka@gmail.com>
2022-04-12 14:31:12 +0200

    Added MetroScreen

---
+++ core/src/com/oxology/findyourway/screens/MetroScreen.java
@@ -0,0 +1,52 @@
+package com.oxology.findyourway.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.oxology.findyourway.FindYourWay;
+import com.oxology.findyourway.GameData;
+
+public class MetroScreen implements Screen {
+    FindYourWay game;
+
+    public MetroScreen(FindYourWay game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(1, 1, 1, 1);
+        game.batch.begin();
+        game.batch.draw(GameData.ARROW_LEFT, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
