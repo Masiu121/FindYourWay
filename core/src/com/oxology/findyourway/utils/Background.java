f5e92b8f0b2e5fc104b9e824382dc7621935eb63
Masiu121 <maks.kapolka@gmail.com>
2022-04-12 07:58:45 +0200

    Added Background

---
+++ core/src/com/oxology/findyourway/utils/Background.java
@@ -0,0 +1,31 @@
+package com.oxology.findyourway.utils;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.oxology.findyourway.GameData;
+import com.oxology.findyourway.world.entities.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Background {
+    List<BackgroundElement> levels;
+    Player player;
+
+    public Background(Player player) {
+        this.levels = new ArrayList<>();
+        levels.add(GameData.SUN);
+        levels.add(GameData.GRADIENT);
+        levels.add(GameData.BLOKI_3);
+        levels.add(GameData.BLOKI_2);
+        levels.add(GameData.BLOKI_1);
+        levels.add(GameData.BARIERKI);
+
+        this.player = player;
+    }
+
+    public void update(float deltaTime) {
+        if(player.getxSpeed() != 0) {
+
+        }
+    }
+}
