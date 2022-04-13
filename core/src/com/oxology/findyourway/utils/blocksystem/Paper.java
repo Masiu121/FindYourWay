00786424f6e889cbdf89656fccf5490a7d2e0906
Masiu121 <maks.kapolka@gmail.com>
2022-04-13 21:12:29 +0200

    Updated Paper

--- core/src/com/oxology/findyourway/utils/blocksystem/Paper.java
+++ core/src/com/oxology/findyourway/utils/blocksystem/Paper.java
@@ -3,13 +3,18 @@ package com.oxology.findyourway.utils.blocksystem;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.oxology.findyourway.FindYourWay;
 import com.oxology.findyourway.GameData;
 import com.oxology.findyourway.utils.blocksystem.blocks.Block;
+import com.oxology.findyourway.utils.blocksystem.blocks.StartBlock;
+import com.oxology.findyourway.utils.blocksystem.blocks.StopBlock;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Paper {
+    private FindYourWay game;
+
     private List<Block> blocks;
     private TextCard card;
 
@@ -17,7 +22,9 @@ public class Paper {
 
     private float x, y;
 
-    public Paper() {
+    public Paper(FindYourWay game) {
+        this.game = game;
+
         card = new TextCard(1f , GameData.TEXT_CARD, "sdaasd");
 
         this.blocks = new ArrayList<>();
@@ -32,12 +39,28 @@ public class Paper {
         if(visible) {
             batch.draw(GameData.PAPER, x, y);
             card.draw(batch, x - 111, 70);
+
+            for(Block block : blocks) {
+                block.draw(batch);
+            }
         }
     }
 
     public void update(float deltaTime) {
         if(Gdx.input.isKeyJustPressed(Input.Keys.P)){
             visible = !visible;
+        }
+
+        if(Gdx.input.isKeyJustPressed(Input.Keys.M)) {
+            addBlock(new StartBlock((int) x, (int) y, this, game));
+        }
+
+        if(Gdx.input.isKeyJustPressed(Input.Keys.M)) {
+            addBlock(new StopBlock((int) x, (int) y, this, game));
+        }
+
+        for(Block block : blocks) {
+            block.update();
         }
     }
 
