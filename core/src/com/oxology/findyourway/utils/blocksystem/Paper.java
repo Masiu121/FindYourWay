53c36d3b7282d10c93c8eca4a71cfc00d560a330
Masiu121 <maks.kapolka@gmail.com>
2022-04-13 20:01:55 +0200

    Updated Paper

--- core/src/com/oxology/findyourway/utils/blocksystem/Paper.java
+++ core/src/com/oxology/findyourway/utils/blocksystem/Paper.java
@@ -4,26 +4,48 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.oxology.findyourway.GameData;
+import com.oxology.findyourway.utils.blocksystem.blocks.Block;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Paper {
+    private List<Block> blocks;
+    private TextCard card;
 
-    public boolean drawPaper;
+    private boolean visible;
 
-    public Paper(boolean drawPaper) {
-        this.drawPaper = drawPaper;
+    private float x, y;
+
+    public Paper() {
+        card = new TextCard(1f , GameData.TEXT_CARD, "sdaasd");
+
+        this.blocks = new ArrayList<>();
     }
 
-    public void draw(SpriteBatch batch , float x , float y) {
-        batch.draw(GameData.PAPER, x, y);
+    public void setPos(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(SpriteBatch batch) {
+        if(visible) {
+            batch.draw(GameData.PAPER, x, y);
+            card.draw(batch, x - 111, 70);
+        }
     }
 
     public void update(float deltaTime) {
         if(Gdx.input.isKeyJustPressed(Input.Keys.P)){
-            drawPaper = !drawPaper;
+            visible = !visible;
         }
     }
 
-    public boolean isDrawPaper() {
-        return drawPaper;
+    public void addBlock(Block block) {
+        blocks.add(block);
+    }
+
+    public boolean isVisible() {
+        return visible;
     }
 }
