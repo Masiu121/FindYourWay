5bdc368c6d1b4847967710ce5ff975479ad6ed4d
Masiu121 <maks.kapolka@gmail.com>
2022-04-13 20:01:22 +0200

    Updated Block

--- core/src/com/oxology/findyourway/utils/blocksystem/blocks/Block.java
+++ core/src/com/oxology/findyourway/utils/blocksystem/blocks/Block.java
@@ -1,4 +1,11 @@
 package com.oxology.findyourway.utils.blocksystem.blocks;
 
-public class Block {
+import com.badlogic.gdx.graphics.Texture;
+import com.oxology.findyourway.FindYourWay;
+import com.oxology.findyourway.utils.GameObject;
+
+public class Block extends GameObject {
+    public Block(float x, float y, Texture texture, float scale, FindYourWay game) {
+        super(x, y, texture, scale, game);
+    }
 }
